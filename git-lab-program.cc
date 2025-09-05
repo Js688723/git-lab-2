8ded1b9c4a8f3f9e76fc7bea520572409245b53c
Jerry Stratton <Js688723@ohio.edu>
2025-09-05 15:48:40 -0400

    HI

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jerry Stratton
+ *        Date: Sep 5, 2025
+ * Description: Prints Hello Git!!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
